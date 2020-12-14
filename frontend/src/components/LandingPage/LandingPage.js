d3223ec1450090ec29d7cebbd3df0e98cb9949a8
smartshk <seohkim@student.42seoul.kr>
2020-12-14 23:29:02 +0900

    add modal to update sugang info

--- frontend/src/components/LandingPage/LandingPage.js
+++ frontend/src/components/LandingPage/LandingPage.js
@@ -1,8 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import { Card, Avatar, Col, Typography, Row } from 'antd';
 import { Menu, Dropdown, Button } from 'antd';
 import axios from 'axios';
 import { USER_SERVER } from '../Config';
+import SugangList from './SugangList';
+import { Modal } from 'antd';
+import { Input } from 'antd';
+const { TextArea } = Input;
 const { Title } = Typography;
 
 // 로그인을 안했을 시에는, 로그인을 해야 이용이 가능하다고 표시
@@ -16,7 +20,20 @@ const { Title } = Typography;
 function LandingPage() {
   const [Sugang, setSugang] = useState([]);
   const [TableNumber, setTableNumber] = useState(1);
+  const [isModalVisible, setIsModalVisible] = useState(false);
   const [Ha, setHa] = useState([]);
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
 
   const menu = (
     <Menu>
@@ -44,19 +61,10 @@ function LandingPage() {
           Sugang.push({
             sugang: sugang,
             professor: res.data.professor,
-          })
+          });
           setSugang(Sugang);
-          setHa(Sugang.map((sugang, index) => {
-            return (
-              <Col key={index} lg={6} md={8} xs={24}>
-                <span id>과목명: {sugang.sugang.subject_name} </span>
-                <br />
-                <span>교수: {sugang.professor} </span>
-                <hr />
-              </Col>
-            );
-        }));
-        });
+          setHa(Sugang.map((sugang) => {}));
+    });
   }
 
   async function test(arr) {
@@ -64,7 +72,7 @@ function LandingPage() {
         await delay(sugang);
     }
   }
-  
+
   useEffect(() => {
     axios
       .get(`http://localhost:4000/sugang/get/` + localStorage.getItem('userId'))
@@ -78,6 +86,43 @@ function LandingPage() {
       });
   }, []);
 
+  const renderCards = Sugang.map((sugang, index) => {
+    if (sugang.sugang.timetable_number === TableNumber) {
+      return (
+        <Col key={index} lg={6} md={8} xs={24}>
+          <span id>과목명: {sugang.sugang.subject_name} </span>
+          <br />
+          <span>교수: {sugang.professor} </span>
+          <Button onClick={showModal}>수정</Button>
+          <Modal
+        title="수정하기"
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+          {/* 여기를 sugang 정보 수정한 것을 state 로 가지고 있어야 함 -> 그리고 서버 post 요청 보내기 */}
+        <TextArea rows={4} />
+      </Modal>
+            <Button>삭제</Button>
+          <hr />
+        </Col>
+      );
+        }
+  })
+
+  const renderCards_dropping = Sugang.map((sugang, index) => {
+    if (sugang.sugang.timetable_number === TableNumber && sugang.sugang.wanna_drop === 1) {
+      return (
+        <Col key={index} lg={6} md={8} xs={24}>
+          <span id>과목명: {sugang.sugang.subject_name} </span>
+          <br />
+          <span>교수: {sugang.professor} </span>
+          <hr />
+        </Col>
+      );
+    }
+  });
+
   return (
     <div style={{ width: '85%', margin: '3rem auto' }}>
       <Title level={2}>내가 갖고있는 과목</Title>
@@ -87,19 +132,26 @@ function LandingPage() {
       <hr />
       <Row gutter={16}>
           {Sugang.length === 0 ?
-            <div style={{ display: 'flex', height: '300px', justifyContent: 'center', alignItems: 'center' }}>
-              <h2>No post yet...</h2>
-            </div> :
-            <div>
-            <Row gutter={16}>
-              {Ha}
-            </Row>
-            </div>
+            (<div style={{ display: 'flex', height: '300px', justifyContent: 'center', alignItems: 'center' }}>
+              <h2>No subjects yet...</h2>
+            </div> ):
+            (<>
+                    {renderCards}
+            </>)
           }
       </Row>
-      <Title level={2}>내가 버리고 싶은 과목 </Title>
+      <Title level={2}> 내가 버리고 싶은 과목 </Title>
       <hr />
-      {/* <Row gutter={16}>{renderCards_dropping}</Row> */}
+      <Row gutter={16}>
+          {Sugang.length === 0 ?
+            (<div style={{ display: 'flex', height: '300px', justifyContent: 'center', alignItems: 'center' }}>
+              <h2>No subjects yet...</h2>
+            </div> ):
+            (<>
+                    {renderCards_dropping}
+            </>)
+          }
+      </Row>
     </div>
   );
 }
